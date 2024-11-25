8b63dd8bf66ed767b38a64b468293b387160bf74
Edi Mendoza <ntarticpnguin@proton.me>
2024-11-25 05:06:41 -0600

    improvement, missing urls

--- labme.sh
+++ labme.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
 # fetch name for venv
-
 read -p "Enter the name of the new repo: " name
 
 # Create directory and navigate into it:
 mkdir "$name"
-cd "$name"
+cd "$name" || exit
+mkdir data
+touch 01_eda.ipynb
 
 # Create venv
 python -m venv venv
 
 # Activate venv
-source venv/bin/activate
+source venv/bin/activate || { echo "Failed to activate venv"; exit 1; }
 
 # upgrade pip
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install the required packages
-pip install pandas jupyter matplotlib numpy seaborn
+pip install pandas jupyterlab matplotlib numpy seaborn
+
+# check if jupyter exists in path and attempt reinstall
+if ! command -v jupyter &> /dev/null
+then
+    echo "JupyterLab installation failed. Attempting to reinstall."
+    pip install jupyterlab
+fi
 
 git init
 
 echo "Virtual environment '$name' created and activated."
-echo "Packages installed: pandas, jupyter, matplotlib, numpy seaborn"
+echo "Packages installed: pandas, jupyterlab, matplotlib, numpy seaborn"
 echo "git tracking initiated"
+
+# Run jupyter lab in the background and capture url
+nohup jupyter lab &> nohup.out &
+
+# to give time for server to start and fetch urls
+sleep 5
+
+# Print jupyter lab url
+jupyter notebook list
+
+
+echo "Jupyter Lab running in the background. Access it at the URL above."
+
